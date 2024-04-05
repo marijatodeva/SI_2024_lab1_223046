1c9c22915558d4c99cb4eefe42ca337a16351096
Marija Todeva <marijatodeva86@gmail.com>
2024-04-05 14:00:05 +0200

    Fixed line endings in SILab1_2.java

--- SILab1_2.java
+++ SILab1_2.java
@@ -5,10 +5,18 @@ public class SILab1_2 {
     private static String makeBigStringFrom (List<String> list, int minLength) {
         StringBuilder sb = new StringBuilder();
         for (String el : list) {
+ HEAD
             if (el.length() < minLength) {
+=======
+            if (el.length() > minLength) {
+>>>>>>> 65ca155 (Fixed line endings in SILab1_2.java)
                 sb.append(el);
             }
         }
         return sb.toString();
     }
+HEAD
 }
+=======
+}
+>>>>>>> 65ca155 (Fixed line endings in SILab1_2.java)
